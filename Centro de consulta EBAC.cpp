cab3414f514bdde5aa69ead30761a12df3e0464d
ianfujii <137218678+ianfujii@users.noreply.github.com>
2023-06-21 11:14:53 -0300

    Opção de saída
    Foi incluso no sistema a opção de saída para o usuário

--- Centro de consulta EBAC.cpp
+++ Centro de consulta EBAC.cpp
@@ -132,7 +132,8 @@ int main()
 	printf("Escolha a opção desejada do menu:\n\n");
 	printf("\t1- Registrar nomes\n");
 	printf("\t2- Consultar nomes\n");
-	printf("\t3- Deletar nomes\n\n"); 
+	printf("\t3- Deletar nomes\n"); 
+	printf("\t4- Sair do sistema\n\n");
 	printf("Digite a opção desejada:"); // Fim do menu
 	
 	scanf("%d", &opcao); // Opções de escolha do usuário
@@ -153,6 +154,11 @@ int main()
 		 case 3:
 		 deletar();
 		break;	
+		
+		case 4:
+			 printf("Obrigado por utilizar o sistema!\n");
+		 return 0;
+		 break;
 		
 
 		default:
